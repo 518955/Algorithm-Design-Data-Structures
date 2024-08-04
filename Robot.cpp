d86553d1e6e2b3ef73bf61f9b9e41a54324aab69
Ruiyuan Li/a1881820 <141195863+518955@users.noreply.github.com>
2024-08-04 11:03:12 +0800

    Delete Robot.cpp

--- Robot.cpp
+++
@@ -1,7 +0,0 @@
-#include "Robot.h"
-#include "Pirate.h"
-#include "Ninja.h"
-
-bool Robot::beats(const Move* other) const {
-    return dynamic_cast<const Pirate*>(other) || dynamic_cast<const Ninja*>(other);
-}
