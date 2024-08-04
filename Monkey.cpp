c0d98a90987d9a02f66b60b4c88772215ffacec2
Ruiyuan Li/a1881820 <141195863+518955@users.noreply.github.com>
2024-08-04 11:02:30 +0800

    Delete Monkey.cpp

--- Monkey.cpp
+++
@@ -1,9 +0,0 @@
-#include "Monkey.h"
-#include "Robot.h"
-#include "Pirate.h"
-#include "Ninja.h"
-#include "Zombie.h"
-
-bool Monkey::beats(const Move* other) const {
-    return dynamic_cast<const Robot*>(other) || dynamic_cast<const Zombie*>(other);
-}
