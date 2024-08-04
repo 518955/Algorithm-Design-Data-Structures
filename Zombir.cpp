daa85621b7386d1fc7805f928aca4f74201a84d1
Ruiyuan Li/a1881820 <141195863+518955@users.noreply.github.com>
2024-08-04 11:03:46 +0800

    Delete Zombir.cpp

--- Zombir.cpp
+++
@@ -1,7 +0,0 @@
-#include "Zombie.h"
-#include "Robot.h"
-#include "Monkey.h"
-
-bool Zombie::beats(const Move* other) const {
-    return dynamic_cast<const Robot*>(other) || dynamic_cast<const Monkey*>(other);
-}
