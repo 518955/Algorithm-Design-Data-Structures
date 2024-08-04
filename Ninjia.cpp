2df1273a4590b4f5edb3a6022d10da1e58c66e77
Ruiyuan Li/a1881820 <141195863+518955@users.noreply.github.com>
2024-08-04 11:02:03 +0800

    Delete Ninjia.cpp

--- Ninjia.cpp
+++
@@ -1,7 +0,0 @@
-#include "Ninja.h"
-#include "Pirate.h"
-#include "Zombie.h"
-
-bool Ninja::beats(const Move* other) const {
-    return dynamic_cast<const Pirate*>(other) || dynamic_cast<const Zombie*>(other);
-}
