d6a0cce0fde8bb5802865e24861e21b30b81c234
Ruiyuan Li/a1881820 <141195863+518955@users.noreply.github.com>
2024-08-04 11:03:53 +0800

    Delete Pirate.cpp

--- Pirate.cpp
+++
@@ -1,7 +0,0 @@
-#include "Pirate.h"
-#include "Monkey.h"
-#include "Ninja.h"
-
-bool Pirate::beats(const Move* other) const {
-    return dynamic_cast<const Monkey*>(other) || dynamic_cast<const Ninja*>(other);
-}
